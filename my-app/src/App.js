e7d0e3278020baa3ade7fd91ba8d21f64384a429
Razzorr <kviraj223@gmail.com>
2024-10-01 10:34:17 -0400

    finished sprint 1
    added navbar with about us page, with all team members + pictures. in navbar, i also added a feedback page and contact page along with a home page as well.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AboutUs from './components/AboutUs';
+import Home from './components/Home';
+import Contact from './components/Contact'; 
 import './App.css';
+import Feedback from './components/Feedback';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>  
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ my-app/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'; 
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to the GT Marketplace</h1>
+      <p>Your one-stop solution for buying and selling items efficiently.</p>
+    </div>
+  );
+}
+
+export default Home;
