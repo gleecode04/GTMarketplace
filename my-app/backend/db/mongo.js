c73b9d3f1ff334fb5f09bbecc7d869f525ddb01f
YIN Tianshu <yintianshu@qq.com>
2024-10-15 13:49:07 -0400

    update

--- my-app/backend/db/mongo.js
+++ my-app/backend/db/mongo.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const mongoSetup = async () => {
     try {
-        const connection = await mongoose.connect('mongodb+srv://TRY:GT-MarketplaceDB@marketplacedb.gaqm6.mongodb.net/', {
+        const connection = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
